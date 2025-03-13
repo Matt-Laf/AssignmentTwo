b62a499deb96f1b4869c58c540bf7d7429ba00cd
Matthew Lafleur <matthewlafleur96@gmail.com>
2025-03-12 20:17:57 -0400

    ?

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./GlobalStyles.css";
 import PaintingOne from "./PaintingOne";
 import Landing from "./landing";
